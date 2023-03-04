b308d0610999c92439be4d30d562cf93d49885e0
XIII <minibrusp@gmail.com>
2023-03-05 01:30:05 +0800

    added theme local storage

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -6,10 +6,20 @@ const ThemeContextProvider = ({children}) => {
 
     const [isDarkTheme, setIsDarkTheme] = useState(false)
 
+    useEffect(() => {
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+            setIsDarkTheme(true)
+        } else {
+            document.documentElement.classList.remove('dark')
+            setIsDarkTheme(false)
+        }
+    }, [])
 
     useEffect(() => {
-        let x = document.documentElement
-        x.classList.toggle('dark')
+        let app = document.documentElement
+        isDarkTheme ? app.classList.add('dark') : app.classList.remove('dark')
+        localStorage.setItem('color-theme', isDarkTheme ? 'true' : false)
       }, [isDarkTheme])
 
     return (
